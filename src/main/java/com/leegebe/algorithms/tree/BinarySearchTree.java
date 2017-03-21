afdef49062a0536089b67e091f43d502c475ded5
lienquan <li.enquan@163.com>
2017-03-21 17:54:16 +0800

    二叉搜索树

---
+++ src/main/java/com/leegebe/algorithms/tree/BinarySearchTree.java
@@ -0,0 +1,27 @@
+package com.leegebe.algorithms.tree;
+
+/**
+ * 二叉搜索树
+ */
+public class BinarySearchTree {
+
+    TreeNode rootNode;
+
+    public TreeNode insert(TreeNode node){
+        if(this.rootNode == null){
+            this.rootNode = node;
+        }else{
+            if(node.getData() > rootNode.getData()){
+
+            }
+        }
+        return rootNode;
+    }
+
+
+
+
+
+
+
+}
