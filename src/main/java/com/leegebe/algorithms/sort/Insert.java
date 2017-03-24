81c72f9faf5377b3e8451e335eb677c1637aa2e3
leegebe <lienquan1988@163.com>
2017-03-24 14:22:56 +0800

    插入排序说明

--- src/main/java/com/leegebe/algorithms/sort/Insert.java
+++ src/main/java/com/leegebe/algorithms/sort/Insert.java
@@ -2,6 +2,9 @@ package com.leegebe.algorithms.sort;
 
 /**
  * 插入排序
+ * 插入排序主要思想
+ * 对第n个元素，假设前面n-1个元素已经有序，从第n-1个元素开始比较，如果该元素小于n-1的元素，互换，继续向前比较；否则此次比较结束。
+ * 最坏情况，数组本身为倒叙，则最后一个元素，需要比较n-1次，交换n-1次；第二个元素比较1次，交换1次，时间复杂度为O(n2)
  */
 public class Insert extends Template {
 
@@ -21,6 +24,7 @@ public class Insert extends Template {
         Integer[] array = {1, 2, 3, 4, 0, 98, 9, 6, 2, 4, 182};
         Template insert = new Insert();
         insert.sort(array);
+        System.out.println(insert.isSorted(array));
         insert.show(array);
     }
 }
